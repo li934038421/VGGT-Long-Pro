e493db6654fcf7cffbc7adbc9617da869bba93a0
Li Haolun <934038421@qq.com>
2026-01-29 18:08:05 +0900

    fix import path error

--- base_models/vggt/models/vggt.py
+++ base_models/vggt/models/vggt.py
@@ -8,10 +8,10 @@ import torch
 import torch.nn as nn
 from huggingface_hub import PyTorchModelHubMixin  # used for model hub
 
-from vggt.models.aggregator import Aggregator
-from vggt.heads.camera_head import CameraHead
-from vggt.heads.dpt_head import DPTHead
-from vggt.heads.track_head import TrackHead
+from base_models.vggt.models.aggregator import Aggregator
+from base_models.vggt.heads.camera_head import CameraHead
+from base_models.vggt.heads.dpt_head import DPTHead
+from base_models.vggt.heads.track_head import TrackHead
 
 
 class VGGT(nn.Module, PyTorchModelHubMixin):
